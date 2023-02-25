015061821db88f3a590a5debc520fa49ca0f0bc7
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-25 10:11:46 -0300

    chore: add eslint

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,8 +1,8 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   body {
     background: #151F2B;
     font-family: 'Montserrat', sans-serif;
   }
-`
+`;
