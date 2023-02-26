b6836653bed3ed0fa3dd55851449db7a7e6ef479
Rafael Anacleto <rafael.anacleto12@gmail.com>
2023-02-25 23:52:34 -0300

    feat: create function to get point control historic

--- src/pages/PointControl/index.tsx
+++ src/pages/PointControl/index.tsx
@@ -1,48 +1,55 @@
+import { useEffect, useRef, useState } from 'react';
+import { api } from '../../libraries/axios';
+import { PointControlHistoricFormattedType } from '../../types/PointControlHistoricFormattedType';
 import { Container } from './styles';
 
 export default function PointControl() {
+  const [pointControlHistoric, setPointControlHistoric] = useState<
+  PointControlHistoricFormattedType[]
+  >([]);
+
+  async function handleGetPointControlHistoric() {
+    const response = await api.get(
+      '/point-control-historic/074df31f-8d81-4e02-876e-621ab63d19de'
+    );
+
+    setPointControlHistoric(response.data);
+  }
+
+  const { current: pointControlHistoricCurrent } = useRef(pointControlHistoric);
+
+  useEffect(() => {
+    handleGetPointControlHistoric();
+  }, [pointControlHistoricCurrent]);
+
   return (
     <Container>
       <header>
-        <div className='clock'>
+        <div className="clock">
           <h5>Relógio de ponto</h5>
 
-          <div className='codeName'>
+          <div className="codeName">
             <span>#ABCD123</span>
-            <span className='codeName__name'>Anacleto</span>
+            <span className="codeName__name">Anacleto</span>
           </div>
         </div>
 
-        <div className='hour'>
-          <span className='hour__time'>0h00m</span>
+        <div className="hour">
+          <span className="hour__time">0h00m</span>
           <span>Horas de hoje</span>
         </div>
       </header>
 
       <button>Hora de (entrada/saída)</button>
 
-      <div className='historicContainer'>
+      <div className="historicContainer">
         <h5>Dias anteriores</h5>
-        <div className='historic'>
-          <span className='historic__date'>03/03/2023</span>
-          <span>7h30</span>
-        </div>
-        <div className='historic'>
-          <span className='historic__date'>03/03/2023</span>
-          <span>7h30</span>
-        </div>
-        <div className='historic'>
-          <span className='historic__date'>03/03/2023</span>
-          <span>7h30</span>
-        </div>
-        <div className='historic'>
-          <span className='historic__date'>03/03/2023</span>
-          <span>7h30</span>
-        </div>
-        <div className='historic'>
-          <span className='historic__date'>03/03/2023</span>
-          <span>7h30</span>
-        </div>
+        {pointControlHistoric.map(pointControl => (
+          <div key={pointControl.id} className="historic">
+            <span className="historic__date">{pointControl.day_worked}</span>
+            <span>{pointControl.worked_hours}</span>
+          </div>
+        ))}
       </div>
     </Container>
   );
